2d9b427b0ace647d65d87dce186d2912cd8871ba
astand <ast.and@bk.ru>
2022-12-11 21:57:02 +0100

    DTimer comments update.

--- src/etc/timers/d-timer.h
+++ src/etc/timers/d-timer.h
@@ -11,81 +11,59 @@ using namespace Timers;
 
 class Timer : private TickerUp<void*> {
  public:
-  /**
-   * @brief Construct a new Timer object
-   * Sets followed timer parameters:
-   * - stopped.
-   * - interval = 0, can be started only by explicit call 'Start(n)'
-   * - not repeatable.
-   */
+  /// @brief Default empty constuctor (stopped, interval 0, non repeat)
   Timer() = default;
 
-  /**
-   * @brief Construct a new Timer object with parameters
-   *
-   * @param intv interval
-   * @param start is start at the instantiation moment
-   * @param repeat is it repeatable timer
-   */
+  /// @brief Detailed timer constructor
+  /// @param intv interval to repeat
+  /// @param start start immediately
+  /// @param repeat repeatable or not
   explicit Timer(interval_t intv, bool start = true, bool repeat = true);
 
+  /// @brief Start timer with new interval
+  /// @param interval new timer's interval
+  /// @return is timer started
   bool Start(interval_t interval);
+
+  /// @brief Start timer with new interval and repeat setup
+  /// @param interval new timer's interval
+  /// @param repeat repeat setup
+  /// @return is timer started
   bool Start(interval_t interval, bool repeat);
 
+  /// @brief Restart timer with current configuration
+  /// @return is timer started
   bool Restart();
 
-  /**
-   * @brief Checks if previously started or repeatedely run
-   * timer is elapsed
-   *
-   * @return true if elapse conditions are fullfilled
-   */
+  /// @brief Checks if the timer elapsed
+  /// @return is timer elapsed
   bool Elapsed();
 
-  /**
-   * @brief Checks if timer is running (active) and elapse event
-   * is expected
-   *
-   * @return true if elapse event is expected, always true for repeatable timers
-   */
+  /// @brief Checks if timer is run (active)
+  /// @return is timer active
   bool IsActive() const {
     return (is_active_);
   }
 
-  /**
-   * @brief Returns number of ticks to the next elapsing event
-   *
-   * @return uint32_t n: number of ticks to elapse event.
-   *                  0: either timer is stopped or elapse event has already been set
-   */
+  /// @brief Number of ticks to next elapsing
+  /// @return 0 if timer is stopped or active elapse event
   uint32_t Ticks() const;
 
-  /**
-   * @brief Sets internal state to Elapsed at the moment
-   *
-   */
+  /// @brief Set elapsed forcibly
   void ForceElapse() {
     AdjustInterval(now() - interval_);
   }
 
+  /// @brief Stop timer
   void Stop() {
     is_active_ = false;
   }
 
  private:
-  /**
-   * @brief Fixes current system ticks counter
-   */
   void FixCurrentTicks() {
     AdjustInterval(now());
   }
 
-  /**
-   * @brief Set the Intv object sets new interval_ value with test on 0
-   *
-   * @param interval new interval value
-   * @return was interval_ updated or not
-   */
   bool SetIntv(interval_t interval) {
     if (interval > 0) {
       interval_ = interval;
@@ -95,23 +73,12 @@ class Timer : private TickerUp<void*> {
     return false;
   }
 
-  /**
-   * @brief Gets the gap to next 'elapse' event
-   *
-   * @param liveticks
-   * @return interval_t gap in ticks for active and not overruned timer (otherwise 0)
-   */
   interval_t GetTicksToNextElapse(systick_t liveticks) const {
     // check if current tick (@liveticks) is not further than inteval_
     systick_t passed_ticks = liveticks - freeze_ticks_;
     return (interval_ > passed_ticks) ? (interval_ - passed_ticks) : (0);
   }
 
-  /**
-   * @brief Set freeze_ticks to new value.
-   *
-   * @param now_ticks new freeze_tick value
-   */
   void AdjustInterval(Timers::TickerCounter::systick_t now_ticks) {
     freeze_ticks_ = now_ticks;
   }
@@ -123,10 +90,10 @@ class Timer : private TickerUp<void*> {
   bool repeat_ = false;
 
  public:
-
+  /// @brief Non copyable
   Timer(const Timer&) = delete;
   Timer& operator=(const Timer&) = delete;
 
 };
 
-} // namespace
+} // namespace DTimers
--- src/etc/timers/tickerup.h
+++ src/etc/timers/tickerup.h
@@ -4,32 +4,27 @@
 
 namespace Timers
 {
-
-/**
- * @brief Base static class for keeping and processing main system
- * tick
- *
- */
+/// @brief Base static class to process base timers counter
 class TickerCounter {
  public:
+  /// @brief tick counter type
   typedef volatile uint32_t systick_t;
 
+  /// @brief Tick counter increment call
   static void ProcessTick() {
     ++systick__;
   }
 
  protected:
+  /// @brief counter
   static systick_t systick__;
 };
 
-/**
- * @brief Intermediate wrapper for all users of TickerCounter with
- * ability to make Atomic operation, if Atomic argument will
- * make atomic state enabled in its constructor and disable it
- * back in desctructor
- *
- * @tparam Atomic
- */
+/// @brief Intermediate wrapper for all users of TickerCounter with
+/// ability to make Atomic operation, if Atomic argument will
+/// make atomic state enabled in its constructor and disable it
+/// back in desctructor
+/// @tparam Atomic RAII type to fulfill atomicy
 template<class Atomic>
 class TickerUp : public TickerCounter {
  protected:
@@ -43,4 +38,4 @@ class TickerUp : public TickerCounter {
   TickerUp() = default;
 };
 
-} // namespace
+} // namespace Timers
