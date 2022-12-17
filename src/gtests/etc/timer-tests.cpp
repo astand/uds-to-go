8638086deac71628ae34ba8e7e9590e60ca5038c
astand <ast.and@bk.ru>
2022-12-17 23:36:27 +0100

    GTest added to project. Timer tests.

---
+++ src/gtests/etc/timer-tests.cpp
@@ -0,0 +1,83 @@
+#include <etc/timers/d-timer.h>
+
+#include <gtest/gtest.h>
+
+using namespace DTimers;
+
+void TickRootCounter(uint32_t ticks)
+{
+  while (ticks--)
+  {
+    Timers::TickerCounter::ProcessTick();
+  }
+}
+
+// Demonstrate some basic assertions.
+TEST(DTimerTests, GeneralTests)
+{
+  // no explicit construction
+  // Timer d = {1};
+  Timer t0{};
+
+  EXPECT_EQ(t0.Elapsed(), false);
+  t0.Start(10);
+  EXPECT_EQ(t0.Elapsed(), false);
+
+  TickRootCounter(9);
+  EXPECT_EQ(t0.Elapsed(), false);
+
+  // 10th tick should cause elapsed event
+  TickRootCounter(1);
+  EXPECT_EQ(t0.Elapsed(), true);
+  // next elapsed after true must be false
+  EXPECT_EQ(t0.Elapsed(), false);
+
+  // not repeatable timer, no elapsed event
+  TickRootCounter(10);
+  EXPECT_EQ(t0.Elapsed(), false);
+
+  t0.Start(10, true);
+  TickRootCounter(10);
+  EXPECT_EQ(t0.Elapsed(), true);
+
+  TickRootCounter(9);
+  EXPECT_EQ(t0.Elapsed(), false);
+
+  TickRootCounter(1);
+  EXPECT_EQ(t0.Elapsed(), true);
+
+  TickRootCounter(9);
+  t0.Stop();
+  EXPECT_EQ(t0.Elapsed(), false);
+  TickRootCounter(1);
+  EXPECT_EQ(t0.Elapsed(), false);
+  TickRootCounter(5);
+  EXPECT_EQ(t0.Elapsed(), false);
+
+}
+
+
+TEST(DTimerTests, ZeroInterval)
+{
+  Timer t1;
+
+  t1.Start(0);
+
+  EXPECT_EQ(t1.IsActive(), true);
+  EXPECT_EQ(t1.Elapsed(), true);
+  // not repeatable
+  EXPECT_EQ(t1.Elapsed(), false);
+  EXPECT_EQ(t1.IsActive(), false);
+
+  EXPECT_EQ(t1.Ticks(), 0);
+
+  t1.Stop();
+  EXPECT_EQ(t1.IsActive(), false);
+  EXPECT_EQ(t1.Elapsed(), false);
+
+  t1.Start(0, true);
+
+  EXPECT_EQ(t1.Elapsed(), true);
+  EXPECT_EQ(t1.Elapsed(), true);
+  EXPECT_EQ(t1.Elapsed(), true);
+}
