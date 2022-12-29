a95141e8e87fb178463424884e8a51b6498fae70
astand <ast.and@bk.ru>
2022-12-29 22:15:33 +0100

    Fixed interfaces comments

--- src/uds/inc/iso-tp-if.h
+++ src/uds/inc/iso-tp-if.h
@@ -2,8 +2,11 @@
 
 #include "iso-tp-types.h"
 
+/// @brief ISO TP client interface. ISO TP handler calls this interface
+/// on each ISO TP event (confirmation, indication etc)
 class IsoTpClient {
  public:
+  /// @brief ISO TP event context information descriptor
   typedef struct
   {
     const uint8_t* data;
@@ -11,54 +14,43 @@ class IsoTpClient {
     N_TarAddress address;
   } IsoTpInfo;
 
-  virtual void OnIsoEvent(N_Event t, N_Result res, const IsoTpInfo& info) = 0;
+  /// @brief ISO TP event notification
+  /// @param ev event type
+  /// @param res network event result
+  /// @param info context information
+  virtual void OnIsoEvent(N_Event ev, N_Result res, const IsoTpInfo& info) = 0;
 };
 
-/**
- * @brief Interface for ISO TP users
- *
- */
+/// @brief ISO TP implementation main interface. Clients use this
+/// interface to invoke payload transmittion
 class IsoTpImpl {
  public:
-  /**
-   * @brief Send ISO TP send request
-   *
-   * @param data - pointer to date to be send to network
-   * @param length - data length
-   * @return IsoTpResult
-   */
+
+  /// @brief ISO TP request
+  /// @param data pointer to ISO TP payload
+  /// @param length size of ISO TP payload
   virtual IsoTpResult Request(const uint8_t* data, size_t length) = 0;
 };
 
-/**
- * @brief Interface for CAN router for passing CAN frames to client
- *
- */
+/// @brief CAN message receiver interface
 class ICAN_Listener {
  public:
-  /**
-   * @brief Process CAN frame from the bus
-   *
-   * @param data - pointer to data in CAN frame
-   * @param length - length of CAN frame payload
-   * @param msgid - CAN frame message ID
-   */
+
+  /// @brief Callback on CAN message receiving
+  /// @param data pointer to CAN message data
+  /// @param length CAN message payload size
+  /// @param msgid CAN message ID
   virtual void ReadFrame(const uint8_t* data, size_t length, uint32_t msgid) = 0;
 };
 
-/**
- * @brief Interface for sending CAN frame to outside world
- *
- */
+/// @brief CAN message sender interface
 class ICAN_Sender {
  public:
-  /**
-   * @brief Sends CAN frame to the BUS
-   *
-   * @param data - pointer to CAN frame payload
-   * @param length - DLC or data length
-   * @param msgid - CAN frame message ID
-   * @return size_t - length if OK, 0 if failed
-   */
+
+  /// @brief Sends CAN message to CAN line
+  /// @param data pointer to CAN message payload
+  /// @param length size of CAN message payload
+  /// @param msgid CAN message ID
+  /// @return length if OK, 0 if failed
   virtual size_t SendFrame(const uint8_t* data, size_t length, uint32_t msgid) = 0;
 };
